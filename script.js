cf9094f04756051c029ffc9f3d04ab6909fc58d7
Cauan Victor <91569130+CauanDZN@users.noreply.github.com>
2022-03-29 23:40:08 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+// Configurando botão Home
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
+// Configurando fade in de imagens
+
+const players =
+  Array.from(document.querySelectorAll('.js-play-on-screen'));
